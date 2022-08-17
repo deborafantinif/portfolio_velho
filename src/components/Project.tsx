3dcadec61e6efe983f2ac840aad6b535b4cf8407
deborafantinif <deboraferfan@gmail.com>
2022-08-17 16:53:23 -0300

    feat: estrutura da pagina projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,24 @@
-function Project() {
+import styles from './Project.module.css';
+import feedGet from '../assets/feedget.jpg';
 
+function Project() {
+  return (
+    <div className={styles.bloco}>
+      <img src={feedGet} alt="tela do projeto feedget" />
+      <div className={styles.content}>
+        <h2>Feedget</h2>
+        <p>Formulário de feedback em formato de chat</p>
+        <div className={styles.button}>
+          <button>
+            SITE
+          </button>
+          <button>
+            REPOSITÓRIO
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,21 @@
-function Projects() {
+import NavBar from '../components/NavBar';
+import Project from '../components/Project';
+import styles from './Projects.module.css';
 
+function Projects() {
+  return (
+    <div>
+      <NavBar/>
+      <main className={styles.main}>
+        <h1>Projetos</h1>
+        <div className={styles.blocos}>
+          <Project/>
+          <Project/>
+          <Project/>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Projects;
